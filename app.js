07062b53cd9ebdc87d4266cc53212dbc5e17590c
Saumyanshu <saumyasanjay08@gmail.com>
2021-01-24 18:17:57 +0530

    news_website

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 //static files
 app.use(express.static('public'))
